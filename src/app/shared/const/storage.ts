192ce769580f74d2ed9f1714285b5e370f80bf5b
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-06 16:27:42 -0300

    setting up authentication service

---
+++ src/app/shared/const/storage.ts
@@ -0,0 +1,5 @@
+export class Storage {
+   public static TOKEN = "pBbtUF2TBG"
+   public static USER = "Qy6HrzuBQN"
+   public static USERS = "cY6cMoQRCG"
+}
--- src/app/shared/service/authentication.service.ts
+++ src/app/shared/service/authentication.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../model/user.model';
+import {Storage} from '../const/storage'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private token: string | undefined;
+  private loggedInUsername: string | undefined 
+
+  constructor(private http: HttpClient) { }
+
+  public login(user: User) {
+    return this.http.post(`${environment.API_URL}\login`, user).toPromise()
+  }
+
+  public register(user: User) {
+    return this.http.post(`${environment.API_URL}\register`, user).toPromise()
+  }
+
+  public logOut(): void {
+    this.token = undefined
+    this.loggedInUsername = undefined
+    localStorage.removeItem(Storage.USER);
+    localStorage.removeItem(Storage.TOKEN);
+    localStorage.removeItem(Storage.USERS);
+  }
+
+  public storageToken(token: string): void {
+    this.token = token
+    localStorage.setItem(Storage.TOKEN, token);
+  }
+
+  public storageUser(user: User): void {
+    localStorage.setItem(Storage.USER, JSON.stringify(user))
+  }
+
+  public getUserFromLocalStorage(): User | undefined {
+    let user = localStorage.getItem(Storage.USER)
+    if (user)
+      return JSON.parse(user)
+    return undefined
+  }
+
+  public geTokenFromLocalStorage(): User | undefined {
+    let token = localStorage.getItem(Storage.TOKEN)
+    if (token)
+      return JSON.parse(token)
+    return undefined
+  }
 }
