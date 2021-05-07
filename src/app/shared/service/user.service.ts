c09b1850f30fb46f2885e97b404aa1f34ea18392
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 11:24:29 -0300

    created user service

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -1,5 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpResponse } from '../model/http-response.model';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +11,35 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor(private http: HttpClient) { }
+
+  public deleteUser(id: string): Promise<HttpResponse | HttpErrorResponse> {
+    return this.http.delete<HttpResponse>(`${environment.API_URL}/delete/${id}`).toPromise()
+  }
+
+  public getUsers(): Promise<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${environment.API_URL}/get`).toPromise()
+  }
+
+  public getUserById(id:string): Promise<User | HttpErrorResponse> {
+    return this.http.get<User>(`${environment.API_URL}/get/${id}`).toPromise()
+  }
+
+  public saveUser(formData: FormData): Promise<User | HttpErrorResponse> {
+    return this.http.post<User>(`${environment.API_URL}/save`, formData).toPromise()
+  }
+
+  public selectAnUser(email="", username=""): Promise<User | HttpErrorResponse> {
+    return this.http.get<User>(`${environment.API_URL}/select?email=${email}&username=${username}`).toPromise()
+  }
+
+  public updateUser(user: User): Promise<User | HttpErrorResponse> {
+    return this.http.put<User>(`${environment.API_URL}/update`, user).toPromise()
+  }
+
+  public updateProfileImage(formData: FormData, id: string): Observable<HttpEvent<User> | HttpErrorResponse> {
+    return this.http.post<User>(`${environment.API_URL}/update-profile/${id}`, formData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+  }
 }
