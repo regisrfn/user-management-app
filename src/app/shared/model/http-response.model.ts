970543aa0896c4facca67e6fd9b0f2d0aeedaa37
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 13:56:22 -0300

    changed class HttpResponse to interface

--- src/app/shared/model/http-response.model.ts
+++ src/app/shared/model/http-response.model.ts
@@ -1,8 +1,8 @@
-export class HttpResponse {
-    errors: {} | undefined;
-    httpStatus: number | undefined;
-    httpStatusCode:number | undefined;
-    message: string | undefined;
-    reason:string | undefined;
-    timestamp:string | undefined
+export interface HttpResponse {
+    errors: {};
+    httpStatus: number;
+    httpStatusCode:number;
+    message: string;
+    reason:string;
+    timestamp:string
 }
