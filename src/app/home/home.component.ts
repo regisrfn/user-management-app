c4d084846fe82c9eef7621fa4ec554b91afb0c3f
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 17:54:45 -0300

    refactoring home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,6 @@ import { UserService } from '../shared/service/user.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
