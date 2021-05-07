cdcad200bad2c3f132bb89dd283bcc65ea30f2f2
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 14:17:09 -0300

    refactoring user service

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -12,31 +12,31 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  public deleteUser(id: string): Promise<HttpResponse | HttpErrorResponse> {
+  public deleteUser(id: string): Promise<HttpResponse> {
     return this.http.delete<HttpResponse>(`${environment.API_URL}/delete/${id}`).toPromise()
   }
 
-  public getUsers(): Promise<User[] | HttpErrorResponse> {
+  public getUsers(): Promise<User[]> {
     return this.http.get<User[]>(`${environment.API_URL}/get`).toPromise()
   }
 
-  public getUserById(id:string): Promise<User | HttpErrorResponse> {
+  public getUserById(id:string): Promise<User> {
     return this.http.get<User>(`${environment.API_URL}/get/${id}`).toPromise()
   }
 
-  public saveUser(formData: FormData): Promise<User | HttpErrorResponse> {
+  public saveUser(formData: FormData): Promise<User> {
     return this.http.post<User>(`${environment.API_URL}/save`, formData).toPromise()
   }
 
-  public selectAnUser(email="", username=""): Promise<User | HttpErrorResponse> {
+  public selectAnUser(email="", username=""): Promise<User> {
     return this.http.get<User>(`${environment.API_URL}/select?email=${email}&username=${username}`).toPromise()
   }
 
-  public updateUser(user: User): Promise<User | HttpErrorResponse> {
+  public updateUser(user: User): Promise<User> {
     return this.http.put<User>(`${environment.API_URL}/update`, user).toPromise()
   }
 
-  public updateProfileImage(formData: FormData, id: string): Observable<HttpEvent<User> | HttpErrorResponse> {
+  public updateProfileImage(formData: FormData, id: string): Observable<HttpEvent<User>> {
     return this.http.post<User>(`${environment.API_URL}/update-profile/${id}`, formData, {
       reportProgress: true,
       observe: 'events'
