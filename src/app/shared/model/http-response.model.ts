84208e7a534f9da5409aabbea1338878d665588d
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 11:39:37 -0300

    created http response model

---
+++ src/app/shared/model/http-response.model.ts
@@ -0,0 +1,8 @@
+export class HttpResponse {
+    errors: {} | undefined;
+    httpStatus: number | undefined;
+    httpStatusCode:number | undefined;
+    message: string | undefined;
+    reason:string | undefined;
+    timestamp:string | undefined
+}
