c423524929e1a054c4fb9ab67ffb7a5658da8de3
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 11:39:53 -0300

    created user model

--- src/app/shared/model/user.model.ts
+++ src/app/shared/model/user.model.ts
@@ -1,2 +1,15 @@
 export class User {
+    active: boolean | undefined
+    authorities: string[] | undefined
+    createdAt: string | undefined
+    email: string | undefined
+    firstName: string | undefined
+    info: string | undefined
+    lastLoginDate: string | undefined
+    lastName: string | undefined
+    locked: string | undefined
+    role: string | undefined
+    userId: string | undefined
+    userNo: string | undefined
+    username: string | undefined
 }
