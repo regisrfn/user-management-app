7f1f7182e63f0d71e00fed17206f2c1158476b9d
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-07 14:16:43 -0300

    created home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/service/user.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers()
+      .then(res => console.log(res))
+      .catch((err:HttpErrorResponse) => console.log(err.error))
+  }
+
+}
