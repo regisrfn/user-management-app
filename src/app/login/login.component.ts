6fe88b397f1a91eb286744277839db02207c01f6
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-13 17:42:56 -0300

    setting up login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderType } from '../enum/header-type.enum';
 import { NotificationType } from '../enum/notification-type.enum';
 import { User } from '../shared/model/user.model';
 import { AuthenticationService } from '../shared/service/authentication.service';
@@ -27,12 +28,19 @@ export class LoginComponent implements OnInit {
     this.loading = true
     this.authService.login(user)
       .then(res => {
-        this.loading = false
+        const token = res.headers.get(HeaderType.JWT_TOKEN);
+        if (token && res.body) {
+          this.authService.storageToken(token);
+          this.authService.storageUser(res.body);
+          this.authService.loadToken();
+          this.router.navigate(['/home']);
+        }
+        this.loading = false;
       })
       .catch((err: HttpErrorResponse) => {
-        this.notificationService.notify(NotificationType.ERROR, err.error.message)
-        this.loading = false
-      })
+        this.notificationService.notify(NotificationType.ERROR, err.error.message);
+        this.loading = false;
+      });
   }
 
 }
