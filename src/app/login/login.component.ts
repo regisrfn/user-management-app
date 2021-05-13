0869a889e46dc96bdfcffeb5c2456e8219284744
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-13 12:24:09 -0300

    bulding login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationType } from '../enum/notification-type.enum';
+import { User } from '../shared/model/user.model';
+import { AuthenticationService } from '../shared/service/authentication.service';
+import { NotificationService } from '../shared/service/notification.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading = false
+  loginUser = new User
+
+  constructor(private router: Router,
+    private authService: AuthenticationService,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+  }
+
+  public login(user: User) {
+    this.loading = true
+    this.authService.login(user)
+      .then(res => {
+        this.loading = false
+      })
+      .catch((err: HttpErrorResponse) => {
+        this.notificationService.notify(NotificationType.ERROR, err.error.message)
+        this.loading = false
+      })
   }
 
 }
