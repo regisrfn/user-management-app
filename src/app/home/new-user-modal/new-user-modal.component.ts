c0e75e1fa684976ab0beb7d09402278e016c284e
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-19 15:45:15 -0300

    selecting and unselecting files

--- src/app/home/new-user-modal/new-user-modal.component.ts
+++ src/app/home/new-user-modal/new-user-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/shared/enum/notification-type.enum';
 import { User } from 'src/app/shared/model/user.model';
+import { NotificationService } from 'src/app/shared/service/notification.service';
 
 @Component({
   selector: 'app-new-user-modal',
@@ -9,9 +11,38 @@ import { User } from 'src/app/shared/model/user.model';
 export class NewUserModalComponent implements OnInit {
 
   newUser = new User
-  constructor() { }
+  formData = new FormData();
+  previewImgURL: string | ArrayBuffer | null | undefined
+
+  constructor(private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+  }
+
+  public selectFiles(input: HTMLInputElement) {
+    const files = input.files;
+
+    if (files && files[0]) {
+
+      var mimeType = files[0].type;
+      if (mimeType.match(/image\/*/) == null) {
+        this.notificationService.notify(NotificationType.ERROR, "Only images are supported.")
+        return;
+      }
+
+      this.formData.append('file', files[0], files[0].name);
+
+      var reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = (_event) => {
+        this.previewImgURL = reader.result;
+      }
+    }
+  }
+
+  public unselectFiles(){
+    this.formData.delete('file')
+    this.previewImgURL = undefined
   }
 
 }
