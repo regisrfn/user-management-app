df27584559fefa807984a6620409897e246b5bbf
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-05-19 14:51:28 -0300

    bind user to form

--- src/app/home/new-user-modal/new-user-modal.component.ts
+++ src/app/home/new-user-modal/new-user-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/model/user.model';
 
 @Component({
   selector: 'app-new-user-modal',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserModalComponent implements OnInit {
 
+  newUser = new User
   constructor() { }
 
   ngOnInit(): void {
